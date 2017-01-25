a33bebece8bb114cfd25b125a9398fbc688244ba
Greg Lee <gml2136@gmail.com>
2017-01-25 14:05:34 -0800

    started js logic, need to update html

--- assets/javascript/hangman.js
+++ assets/javascript/hangman.js
@@ -1,9 +1,38 @@
 // global declarations
+var MAX_TRIES = 15;
 
 
 
 // game object
+var Hangman = {
+  wins: 0,
+  loses: 0,
+  secretWordArray: ["arrakis", "dune", "", "snow crash", "kindred" ],
+  secretWord: "",
+  guessTotal: 0,
 
+  //methods
+  getSecretWord: function(){ this.secretWord = this.secretWordArray.shift();},
+  secretWordLength: function() { return this.secretWord.length;},
+  //guess methods
+  isCorrectGuess: function(char){
+                    for(var i = 0, l = this.secretWordLength(); i < l; i++){
+                        if (char === this.secretWord[i]) {
+                          return [char, true];
+                        } else if (i === (l - 1)) {
+                          return [char, false];
+                        }
+                    }
+                },
+
+}
 
 
 //calls
+function play() {
+  var Game = Hangman;
+  Game.getSecretWord();
+  var result = Game.isCorrectGuess("b");
+
+  console.log(result);
+};
